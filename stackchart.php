1da6ab696b95d85cc961544bbb15e3f1f0cb1a75
Dario Alvarez <dario.alvarezmiranda@wsu.edu>
2012-01-24 15:29:51 -0800

    Fixed mobile version. Added "browse by library of congress" section

--- stackchart.php
+++ stackchart.php
@@ -1,32 +1,5 @@
-<?
-function is_mobile(){
-	$useragent=$_SERVER['HTTP_USER_AGENT'];
-	//print $useragent;
-	if(preg_match('/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|
-		iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|
-		p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|
-		xda|xiino/i',$useragent) ||
-		preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|
-		al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|
-		bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|
-		co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|
-		em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|
-		go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|
-		hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|
-		jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|
-		libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|
-		mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|
-		n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|
-		p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|
-		qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|
-		sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|
-		sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|
-		tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|
-		veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|
-		webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i',substr($useragent,0,4)))
-		return true;
-	return false;
-	}
+<?php
+	require('common.php');
 
 	function openSectionHeader($request){
 		if (isset($request['openId'])){
@@ -74,7 +47,6 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 <script type="text/javascript" src="javascripts/jquery.nestedAccordion.js"></script>
 <link href="css/stylesheets/li_stylesheet.css" rel="stylesheet" type="text/css" />
 <link href="css/stylesheets/style.css" rel="stylesheet" type="text/css" />
-
 <title>Holland & Terrell Stack Chart</title>
 
 <? //Only use headers if client is not mobile
@@ -86,9 +58,13 @@ if(!is_mobile()){
 <script type="text/javascript" src="http://www.wsulibs.wsu.edu/Header/Header2.js"></script>
 
 <!-- END HEADER -->
-<? } //end if 
- //maybe something for an else here. We'll see.
-
+<? } //end if
+else {
+	//include some mobile specific styling
+?>
+<link href="css/stylesheets/mobile.css" rel="stylesheet" type="text/css" />
+<?	
+} 
 ?> 
 <script language="JavaScript"><!--
 
@@ -125,6 +101,12 @@ $(function() {
       head: "h2, h3", next: "div", showMethod: "slideFadeDown", hideMethod: "slideFadeUp",
       initShow : "div.shown", showSpeed: 200, hideSpeed: 400
     });
+    
+    $("#locClassDiv").n_accordion({obj: "div", wrapper: "div", el: ".h", 
+      head: "h2, h3", next: "div", showMethod: "slideFadeDown", hideMethod: "slideFadeUp",
+      initShow : "div.shown", showSpeed: 200, hideSpeed: 400
+    });
+    
   $("html").removeClass("js");
   <?
   	//check whether we need to open a section after load
@@ -175,7 +157,11 @@ $(function() {
 
 <!-- FRONTIER -->
 
+<? if(!is_mobile()){?>
 <div class="container showgrid">
+<?} else {?>
+<div class="container mobile_con">
+<?}?>	
 
 <?
 //if(!($db=new SQLite3('db/stackchart.sqlite')))
@@ -300,9 +286,8 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 
 ?>
 <div id="mainDiv" >
-	
-	
-	<div class="span-9" >
+		
+	<div class="<? if (is_mobile()) print "mobile_half"; else print "span-9"; ?>">
 		<h1 align="center" >Terrell Library </h1>
 		<br>
 		<div id="terrelDiv">
@@ -335,7 +320,7 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 		</div>		
 	</div>
 	
-	<div class="span-9 last">
+	<div class="<? if (is_mobile()) print "mobile_half"; else print "span-9"; ?>">
 		<h1 align="center">Holland Library </h1>
 		<br>
 		<div id="hollandDiv">
@@ -373,11 +358,11 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 		</div>
 	</div>	
 </div>
-<div style="height:10px; clear: both"></div>
+<div class="separator"></div>
 
-<div class="span-18 last">
+<div class="<? if (is_mobile()) print "mobile_full"; else print "span-18 last"; ?>">
 	<div id="otherDiv">
-		<h2><a href="#" id="other_loc" class="acc_head" > Other Locations in The Library</a></h2>
+		<h2><a href="#" id="other_loc" class="acc_head" >Other Holland and Terrell Locations</a></h2>
 			<div>				
 				<div>				  
 				  <table border="1" cellpadding=2 cellspacing="0">
@@ -393,7 +378,7 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 					  <td width="122">1st Floor</td>
 			        </tr>
 			        <tr>
-			          <td>CJ/Newspaper</td>
+			          <td>Current Journals/Newspapers</td>
 			          <td>Ground Floor</td>
 			          <td width="317"> Microforms</td>
 					  <td width="122">1st Floor</td>
@@ -401,7 +386,7 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 			        <tr>
 			          <td>MASC <font size="-3">(Manuscripts, Archives, and Special Collections)</font></td>
 			          <td>Ground Floor</td>
-			          <td width="317"> Maproom</td>
+			          <td width="317">Map Room</td>
 					  <td width="122">1st Floor</td>
 			        </tr>
 			        <tr>
@@ -415,13 +400,56 @@ function displayStacks($db, $page_size, $floor_id, $element_id){
 			</div>
 	</div>
 </div>
+<div class="separator"></div>
 
-<?
-	if($db){		
-		sqlite_close($db);
-	}
-?>
-<div style="clear: both"></div>
+<div class="<? if (is_mobile()) print "mobile_full"; else print "span-18 last"; ?>">
+	<div id="locClassDiv">
+		<h2><a href="#" id="locClassification" class="acc_head" >Browse by Library of Congress Classification</a></h2>
+			<div>				
+				Click on a letter to see which building and floor a class/subclass is on.&nbsp;<br />
+				<br />
+	            <table width="100%" border="0">
+	              <tr>
+	                <td width="44%"><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/A.html">A - General Works</a></td>
+	                <td width="32%"><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/J.html">J - Political Science</a></td>
+	                <td width="24%"><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/R.html">R - Medicine</a></td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/B.html">B - Philosophy, Psychology, Religion</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/K.html">K - Law</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/S.html">S - Agriculture</a></td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/C.html">C - Auxiliary Sciences of History</a> </td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/L.html">L - Education</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/T.html">T- Technology</a> </td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/D.html">D - World History</a> </td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/M.Html">M - Music and Books on Music</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/U.html">U - Military Science</a></td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/EF.html">E - History of the Americas</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/N.html">N - Fine Arts</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/W.html">W - WSU</a></td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/G.html">G - Geography, Anthropology, Recreation</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/P.html">P - Language and Literature</a></td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/Z.html">Z - Bibliography</a></td>
+	              </tr>
+	              <tr>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/H.html">H - Social Sciences</a> </td>
+	                <td><a href="http://www.wsulibs.wsu.edu/Usered/tutorials/hatmapsstacks/Q.html">Q - Science</a> </td>
+	                <td>&nbsp;</td>
+	              </tr>
+	            </table>
+			</div>
+	</div>
+</div>
+<div class="separator"></div>
+
 <!-- Start of StatCounter Code for Default Guide -->
 <script type="text/javascript">
 var sc_project=7577748;
@@ -436,9 +464,15 @@ target="_blank"><img class="statcounter"
 src="http://c.statcounter.com/7577748/0/0e357b41/1/"
 alt="tumblr hit counter"></a></div></noscript>
 <!-- End of StatCounter Code for Default Guide -->
+</div><!-- End container div -->
 </div>
 <!-- INCLUDE FOOTER FILE HERE        Do Not Edit                                   -->
 <script language="JavaScript" src="javascripts/footers/li_footer.js" type="text/javascript"></script> 
 <!-- END FOOTER HEADER FILE                                               -->
 </body>
 </html>
+<?
+	if($db){		
+		sqlite_close($db);
+	}
+?>
